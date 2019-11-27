0ecd6cb68d0fd05f38e7540d2ab2cebd3d91d698
SharePCg <tutc3212019@gmail.com>
2019-11-28 00:08:03 +0900

    傾きセンサーの機能を改良 Testscript

---
+++ Assets/Script/Cheack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 左に移動
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(-0.001f, 0.0f, 0.0f);
+        }
+        // 右に移動
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(0.001f, 0.0f, 0.0f);
+        }
+        // 前に移動
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(0.0f, 0.0f, 0.001f);
+        }
+        // 後ろに移動
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(0.0f, 0.0f, -0.001f);
+        }
+        // 上に移動
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(0.0f, 0.001f, 0.0f);
+        }
+        // 下に移動
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(0.0f, -0.001f, 0.0f);
+        }
+    }
+}
---
+++ Assets/Script/TestScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uduino;
+
+public class TestScript : MonoBehaviour
+{
+    public float X = 0f;
+    public float Y = 0f;
+    private float tippointX = 0f;
+    private float tippointY = 0f;
+    private float afterpointX = 0f;
+    private float afterpointY = 0f;
+
+    private Vector2 tippoint;
+    private Vector2 afterpoint;
+    private Vector2 difference;
+
+    public float span =1f;
+
+    Collider myCollider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Logging");
+        StartCoroutine("CountTime");
+
+        
+
+        myCollider = this.GetComponent<BoxCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         X= UduinoManager.Instance.analogRead(AnalogPin.A3);
+        Y = UduinoManager.Instance.analogRead(AnalogPin.A4);
+
+
+
+        Debug.Log(Vector2.Distance(tippoint, afterpoint));
+
+        if(Vector2.Distance(tippoint, afterpoint) >=60 )
+        {
+           //Debug.Log("傾きの差は60以上です。");
+            myCollider.enabled = true;
+        }
+        else
+        {
+            
+            myCollider.enabled = false;
+        }
+
+    }
+
+    IEnumerator Logging()
+    {
+        while (true)
+        {
+            yield return null;
+            Vector2 pos1 = new Vector2(X, Y);
+            tippoint = pos1;
+
+
+        }
+    }
+
+    IEnumerator CountTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(span);
+            Vector2 pos2 = new Vector2(X, Y);
+            afterpoint = pos2;
+            
+
+        }
+    }
+
+}
